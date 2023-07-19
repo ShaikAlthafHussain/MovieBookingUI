2b7800df76ddc945c462420de58cc275687c07d8
ShaikAlthafHussain <shaikealthafe@gmail.com>
2023-07-19 17:24:19 +0530

    comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://movieticketbooking20230717181608.azurewebsites.net'};
+  apiBaseUrl: 'https://movieticketbooking20230717181608.azurewebsites.net'
+};
